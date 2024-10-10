401a4242a932fa8b45ba157e8aab24fb955a1314
jooseeruu <arjona2908@gmail.com>
2024-10-10 15:51:30 +0200

    eod

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,4 +1,10 @@
 <?php
 
 echo "<h1>Hello, world!</h1>";
+
+$prueba1 = "Hola\tMundo";
+echo $prueba1;
+
+$prueba2 = 1234;
+echo $prueba2 . "" . $prueba1;
 ?>
---
+++ eod.php
@@ -0,0 +1,9 @@
+<?php
+$texto = <<<EOD
+Este es un ejemplo de texto
+que puede abarcar múltiples líneas
+sin necesidad de escapar comillas "dobles" o 'simples'.
+EOD;
+
+echo $texto;
+?>
