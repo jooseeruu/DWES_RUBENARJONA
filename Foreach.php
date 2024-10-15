583518424c4ea82a2ee8a136200f785f999c0382
jooseeruu <arjona2908@gmail.com>
2024-10-15 19:42:02 +0200

    update

--- Foreach.php
+++ Foreach.php
@@ -1,6 +1,12 @@
 <?php
-$arr = array(1, 2, 3, 4); // indexed array
-foreach ($arr as $value) {
-    echo $value;
+
+$age = array(
+    "Peter" => 35,
+    "Ben" => 37,
+    "Joe" => 43
+);
+
+foreach ($age as $nombre => $edad) {
+    echo "Nombre: " . $nombre . " - Edad: " . $edad . "<br>";
 }
 ?>
