c0e2afa7d523145058a22bfc244bbd8aaedda39d
jooseeruu <arjona2908@gmail.com>
2024-12-08 00:49:20 +0100

    contraseña hash, inserts validados...

--- Ejercicios 181920/connection.php
+++ Ejercicios 181920/connection.php
@@ -18,7 +18,7 @@ function conectarBBDD($tipoDB, $host, $nombreDB, $usuario, $contrasena, $charset
     }
 }
 
-// Función para obtener usuarios de la tabla `usuarios`
+// Función para obtener usuarios de la tabla usuarios
 function getUsersByName($pdo) {
     try {
         $sql = "SELECT * FROM `usuarios`"; // Consulta SQL
@@ -37,8 +37,62 @@ $pdo = conectarBBDD('mysql', 'localhost', 'dwes', 'root', '2908', 'utf8mb4');
 
 if ($pdo) {
     $usuarios = getUsersByName($pdo);
-    echo $usuarios;
+    echo "<pre>"; // Formatear la salida
+    print_r($usuarios);
+    echo "</pre>";
 }
 
-?>
+
+
+// Función para insertar un usuario en la tabla usuarios
+function insertUser($pdo, $nombre, $apellidos, $nombre_usuario, $contrasena, $estado)
+{
+    try {
+        $passwordHash = password_hash($contrasena, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO usuarios (nombre, apellidos, nombre_usuario, contrasena, estado) 
+                VALUES (:nombre, :apellidos, :nombre_usuario, :contrasena, :estado)";
+        $stmt = $pdo->prepare($sql);
+
+        // Asignar valores a los parámetros
+        $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+        $stmt->bindParam(':apellidos', $apellidos, PDO::PARAM_STR);
+        $stmt->bindParam(':nombre_usuario', $nombre_usuario, PDO::PARAM_STR);
+        $stmt->bindParam(':contrasena', $passwordHash, PDO::PARAM_STR);
+        $stmt->bindParam(':estado', $estado, PDO::PARAM_BOOL);
+
+        // Ejecutar la consulta
+        $stmt->execute();
+
+        // Devolvemos el ID del nuevo usuario insertado
+        return $pdo->lastInsertId();
+    } catch (PDOException $e) {
+        echo "Error al insertar usuario: " . $e->getMessage();
+        return false;
+    }
+}
+
+// Insertamos un nuevo usuario
+try {
+    $nuevoUsuario = insertUser($pdo, 'Manolo', 'Gorrion', 'Mago', '4321', true);
+
+    if ($nuevoUsuario) {
+        echo "Nuevo usuario insertado con ID: $nuevoUsuario";
+    } else {
+        echo "No se pudo insertar el usuario por un motivo desconocido.";
+    }
+} catch (PDOException $e) {
+    if ($e->getCode() === 23000) { // código SQLSTATE para error de clave duplicada
+        echo "Error: El nombre de usuario ya está registrado.";
+    } else {
+        // maneja otros errores
+        echo "Error al insertar usuario: " . $e->getMessage();
+    }
+}
+
+return false;
+
+
+
+
+
 
